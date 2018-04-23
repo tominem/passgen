af3cd898e6f1acd661127b0ed93bfe82dc40c86a
tominem@hotmail.com <tominem@hotmail.com>
2018-04-23 17:46:54 -0300

    Alteração de funcionalidade para gerar a senha a partir de um parametro

--- src/main/java/br/com/chubbytech/passgen/App.java
+++ src/main/java/br/com/chubbytech/passgen/App.java
@@ -8,33 +8,60 @@ import java.io.Console;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-    	
-    	Console console = System.console();
-        if (console == null) {
-            System.out.println("Não foi possível abrir o console");
-            System.exit(1);
-        }
 
-        console.printf("%n");
-        console.printf("------------------------------------------------------------------------------------%n");
-        console.printf("  Aplicação para criptografar senhas - Utilitário do NFe-sendmail - ChubbyTech%n");
-        console.printf("------------------------------------------------------------------------------------%n");
-        console.printf("%n");
-        
-        char passwordArray[] = console.readPassword("Entre com a senha a ser criptografada: ");
-        
-        try {
-        	
-			String encrypt = new EncryptDecrypt().encrypt(new String(passwordArray));
-			console.printf("A senha criptografada é: %s%n", encrypt);
-			System.exit(0);
+
+	public static void main( String[] args )
+    {
+		char passwordArrays[] = null;
+    	
+    	if (args != null && args.length > 0) {
 			
-		} catch (Exception e) {
-			console.printf("Ocorreu um erro na tentativa de criptografar a senha");
-			System.exit(2);
-		}
+    		try {
+    			
+				String encrypt = genpass(args[0].toString());
+				System.out.println(encrypt);
+				System.exit(0);
+				
+			} catch (Exception e) {
+				System.err.println("Ocorreu um erro na tentativa de criptografar a senha");
+				System.exit(2);
+			}
+    		
+    		
+		} else {
+    		
+    		Console console = System.console();
+    		
+    		if (console == null) {
+    			System.out.println("Não foi possível abrir o console");
+    			System.exit(1);
+    		}
+    		
+    		console.printf("%n");
+    		console.printf("------------------------------------------------------------------------------------%n");
+    		console.printf("  Aplicação para criptografar senhas - Utilitário do NFe-sendmail - ChubbyTech%n");
+    		console.printf("------------------------------------------------------------------------------------%n");
+    		console.printf("%n");
+    		
+    		passwordArrays = console.readPassword("Entre com a senha a ser criptografada: ");
+    		
+    		try {
+    			
+				String encrypt = genpass(new String(passwordArrays));
+				console.printf("A senha criptografada é: %s%n", encrypt);
+				System.exit(0);
+				
+			} catch (Exception e) {
+				console.printf("Ocorreu um erro na tentativa de criptografar a senha");
+				System.exit(2);
+			}
+    		
+    	}
+    	
     	
     }
+
+	private static String genpass(String pass) throws Exception {
+		return new EncryptDecrypt().encrypt(pass);
+	}
 }
